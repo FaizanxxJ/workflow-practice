b94e1c438b7fa324c34286011e11874d5cc5a837
L1S22BSSE0114 <L1S22BSSE0114@ucp.com>
2025-06-03 09:04:51 +0500

    add setup repo file

---
+++ setupfile.sh
@@ -0,0 +1,13 @@
+echo "setting up repository"
+git init 
+git branch -m main
+git config --local user.name "Faizan"
+git config --local user.email "fazanii092@gmail.com"
+git config --local core.editor notepad
+
+mkdir ./src ./styles ./bin
+touch  ./src/index.html ./styles/index.css ./.gitignore
+
+git remote add #your url 
+
+echo "repository setup successfull !"
